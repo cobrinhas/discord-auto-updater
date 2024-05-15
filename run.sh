6a3580ca0edc5ee8d8d88f69294440d6160f2ed4
freitzzz <1160907@isep.ipp.pt>
2024-05-15 11:16:13 +0100

    bugfix: run cron in root crontab

--- run.sh
+++ run.sh
@@ -3,8 +3,11 @@
 last_version=$(wget 'https://discord.com/api/download?platform=linux&format=deb' --method head -S 2>&1 | grep location | sed -r 's/.*discord-(.*)\.deb/\1/')
 current_version=$(apt-cache show discord | head | grep Version | tr -d 'Version: ')
 
-if [ "$last_version" != "$current_version" ];
-then
-	(cd $(mktemp -d); wget 'https://discord.com/api/download?platform=linux&format=deb' -O discord.deb; sudo apt-get install ./discord.deb)
-	echo "Updated discord to version $last_version."
+if [ "$last_version" != "$current_version" ]; then
+	(
+		cd $(mktemp -d)
+		wget 'https://discord.com/api/download?platform=linux&format=deb' -O discord.deb
+		apt-get install ./discord.deb
+		notify-send "Discord" "Discord has been updated to version $last_version."
+	)
 fi
